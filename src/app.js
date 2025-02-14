250af8643eaa0918a93287c106f100cea4da76e5
0GiS0 <giselatb@outlook.com>
2025-02-14 09:48:50 +0000

    feat: update project and repository names in code scanning endpoint; add work items service and enhance code scanning alerts handling

--- src/app.js
+++ src/app.js
@@ -10,7 +10,56 @@ const app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 
+
+/*
+Retrieves the work items from Azure DevOps for a given project. Pass the GitHub handle as input, and the output will be the name of the work item, its state, 
+the assignee, and a URL in this format: https://dev.azure.com/returngisorg/{project_name}/_workitems/edit/{work_item_id}/
+*/
+
+/* JSON schema
+{
+  "type": "object",
+  "properties": {
+    "username": {
+      "type": "string",
+      "description": "GitHub handle"
+    },
+    "projectName": {
+      "type": "string",
+      "description": "The name of the project on Azure DevOps"
+    }
+  }
+}
+*/
 app.use('/api/workitems', workItemsRoutes);
+
+/*
+Retrieves the code scanning alerts from Azure DevOps for a given project. Pass the GitHub handle as input, and the current file path and the name of the repository.
+*/
+
+/* JSON schema
+{
+  "type": "object",
+  "properties": {
+    "username": {
+      "type": "string",
+      "description": "GitHub handle"
+    },
+    "projectName": {
+      "type": "string",
+      "description": "The name of the project on Azure DevOps"
+    },
+    "repository": {
+      "type": "string",
+      "description": "The name of the workspace or repository"
+    },
+    "currentFilePath": {
+      "type": "string",
+      "description": "The path of the file in the repository"
+    }
+  }
+}
+*/
 app.use('/api/code-scanning', codeScanningRoutes)
 
 module.exports = app;
--- src/controllers/codeScanningController.js
+++ src/controllers/codeScanningController.js
@@ -8,12 +8,13 @@ const getAlerts = async (req, res) => {
     const params = {
         project: req.body.projectName,
         repository: req.body.repository,
+        userName: req.body.username,
+        currentFilePath: req.body.currentFilePath,
     };
 
     const orgUrl = `https://dev.azure.com/${process.env.ADO_ORG}`;
     let authHandler = azdev.getPersonalAccessTokenHandler(process.env.ADO_PAT);
     let connection = new azdev.WebApi(orgUrl, authHandler);
-    const witApi = await connection.getWorkItemTrackingApi();
     const projectApi = await connection.getCoreApi();
 
     projectApi.getProject(params.project).then((project) => {
@@ -24,7 +25,18 @@ const getAlerts = async (req, res) => {
 
     const alerts = await alertApi.getAlerts(params.project, params.repository);
 
+    if (alerts) {
+
+        // Count how many alerts are from CodeQL, how many are from Advanced Security Dependency Scanning
+        const codeQLAlerts = alerts.filter(alert => alert.tool.name === 'CodeQL');
+        const asdsAlerts = alerts.filter(alert => alert.tool.name === 'Advanced Security Dependency Scanning');
+
+        console.log(`CodeQL alerts: ${codeQLAlerts.length}`);
+        console.log(`Advanced Security Dependency Scanning alerts: ${asdsAlerts.length}`);
+    }
+
     res.json(alerts);
+
 };
 
 module.exports = {
